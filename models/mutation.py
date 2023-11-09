8addf7efc386bf449106a5677212aef0ceb5b563
Cristian Chivisky <cristian.chivisky@gmail.com>
2023-11-09 17:57:53 -0300

    Modificaciones en mutation, objects y query

--- models/mutation.py
+++ models/mutation.py
@@ -16,7 +16,25 @@ from .objects import (
     Carrito,
     LineaCarrito,
     Sesion,
-    Usuario
+    Usuario,
+    Autor,
+    Ciudad,
+    DeseoLibro,
+    Direccion,
+    Editorial,
+    Encuadernado,
+    FavoritoLibro,
+    Genero,
+    LibroAutor,
+    LibroEditorial,
+    LibroEncuadernado,
+    LibroGenero,
+    LibroPromocion,
+    LineaPedido,
+    Pedido,
+    PromocionDescuento,
+    Resenia,
+    TipoEnvio
 )
 
 from .libro import Libro as LibroModel
@@ -24,6 +42,24 @@ from .carrito import Carrito as CarritoModel
 from .linea_carrito import LineaCarrito as LineaCarritoModel
 from .usuario import Usuario as UsuarioModel
 from .sesion import Sesion as SesionModel
+from .autor import Autor as AutorModel
+from .ciudad import Ciudad as CiudadModel
+from .deseo_libro import DeseoLibro as DeseoLibroModel
+from .direccion import Direccion as DireccionModel
+from .editorial import Editorial as EditorialModel
+from .encuadernado import Encuadernado as EncuadernadoModel
+from .favorito_libro import FavoritoLibro as FavoritoLibroModel
+from .genero import Genero as GeneroModel
+from .libro_autor import LibroAutor as LibroAutorModel
+from .libro_editorial import LibroEditorial as LibroEditorialModel
+from .libro_encuadernado import LibroEncuadernado as LibroEncuadernadoModel
+from .libro_genero import LibroGenero as LibroGeneroModel
+from .libro_promocion import LibroPromocion as LibroPromocionModel
+from .linea_pedido import LineaPedido as LineaPedidoModel
+from .pedido import Pedido as PedidoModel
+from .promocion_descuento import PromocionDescuento as PromocionDescuentoModel
+from .resenia import Resenia as ReseniaModel
+from .tipo_envio import TipoEnvio as TipoEnvioModel
 
 class createUsuario(Mutation):
     class Arguments:
@@ -126,6 +162,173 @@ class deleteLineaCarrito(Mutation):
 
         return deleteLineaCarrito(linea_carrito=linea_carrito)
 
+class createAutor(Mutation):
+    class Arguments:
+        id_autor = Int(required=True)
+        nombre_autor = String(required=True)
+    
+    autor = Field(lambda: Autor)
+
+    def mutate(self, info, id_autor, nombre_autor):
+        autor = AutorModel(id_autor=id_autor, nombre_autor=nombre_autor)
+
+        db.session.add(autor)
+        db.session.commit()
+        
+        return createAutor(autor=autor)
+
+class createCiudad(Mutation):
+    class Arguments:
+        cp = Int(required=True)
+        nombre_ciudad = String(required=True)
+
+    ciudad = Field(lambda: Ciudad)
+
+    def mutate(self, info, cp, nombre_ciudad):
+        ciudad = CiudadModel(cp=cp, nombre_ciudad=nombre_ciudad)
+
+        db.session.add(ciudad)
+        db.session.commit()
+
+        return createCiudad(ciudad=ciudad)
+
+class createDeseoLibro(Mutation):
+    class Arguments:
+        id_usuario = String(required=True)
+        id_libro = Int(required=True)
+
+    deseo_libro = Field(lambda: DeseoLibro)
+
+    def mutate(self, info, id_usuario, id_libro):
+        deseo_libro = DeseoLibroModel(id_usuario=id_usuario, id_libro=id_libro)
+
+        db.session.add(deseo_libro)
+        db.session.commit()
+
+        return createDeseoLibro(deseo_libro=deseo_libro)
+
+class createEditorial(Mutation):
+    class Arguments:
+        id_editorial = Int(required=True)
+        nombre_editorial = String(required=True)
+    
+    editorial = Field(lambda: Editorial)
+
+    def mutate(self, info, id_editorial, nombre_editorial):
+        editorial = EditorialModel(id_editorial=id_editorial, nombre_editorial=nombre_editorial)
+
+        db.session.add(editorial)
+        db.session.commit()
+        
+        return createEditorial(editorial=editorial)
+
+class createEncuadernado(Mutation):
+    class Arguments:
+        id_encuadernado = Int(required=True)
+        tipo = String(required=True)
+    
+    encuadernado = Field(lambda: Encuadernado)
+
+    def mutate(self, info, id_encuadernado, tipo):
+        encuadernado = EncuadernadoModel(id_encuadernado=id_encuadernado, tipo=tipo)
+
+        db.session.add(encuadernado)
+        db.session.commit()
+        
+        return createEncuadernado(encuadernado=encuadernado)
+
+class createGenero(Mutation):
+    class Arguments:
+        id_genero = Int(required=True)
+        nombre_genero = String(required=True)
+    
+    genero = Field(lambda: Genero)
+
+    def mutate(self, info, id_genero, nombre_genero):
+        genero = GeneroModel(id_genero=id_genero, nombre_genero=nombre_genero)
+
+        db.session.add(genero)
+        db.session.commit()
+        
+        return createGenero(genero=genero)
+
+class createLibroEditorial(Mutation):
+    class Arguments:
+        id_libro = Int(required=True)
+        id_editorial = Int(required=True)
+    
+    libro_editorial = Field(lambda: LibroEditorial)
+
+    def mutate(self, info, id_libro, id_editorial):
+        libro_editorial = LibroEditorialModel(id_libro=id_libro, id_editorial=id_editorial)
+
+        db.session.add(libro_editorial)
+        db.session.commit()
+        
+        return createLibroEditorial(libro_editorial=libro_editorial)
+
+class createLibroGenero(Mutation):
+    class Arguments:
+        id_libro = Int(required=True)
+        id_genero = Int(required=True)
+    
+    libro_genero = Field(lambda: LibroGenero)
+
+    def mutate(self, info, id_libro, id_genero):
+        libro_genero = LibroGeneroModel(id_libro=id_libro, id_genero=id_genero)
+
+        db.session.add(libro_genero)
+        db.session.commit()
+        
+        return createLibroGenero(libro_genero=libro_genero)
+
+class createLibroPromocion(Mutation):
+    class Arguments:
+        id_libro = Int(required=True)
+        id_promocion_descuento = Int(required=True)
+    
+    libro_promocion = Field(lambda: LibroPromocion)
+
+    def mutate(self, info, id_libro, id_promocion_descuento):
+        libro_promocion = LibroPromocionModel(id_libro=id_libro, id_promocion_descuento=id_promocion_descuento)
+
+        db.session.add(libro_promocion)
+        db.session.commit()
+        
+        return createLibroPromocion(libro_promocion=libro_promocion)
+
+class createResenia(Mutation):
+    class Arguments:
+        texto = String(required=True)
+        valoracion = Int(required=True)
+        id_usuario = String(required=True)
+        id_libro = Int(required=True)
+    
+    resenia = Field(lambda: Resenia)
+
+    def mutate(self, info, texto, valoracion, id_usuario, id_libro):
+        resenia = ReseniaModel(texto=texto, valoracion=valoracion, id_usuario=id_usuario, id_libro=id_libro)
+
+        db.session.add(resenia)
+        db.session.commit()
+        
+        return createResenia(resenia=resenia)
+
+class createTipoEnvio(Mutation):
+    class Arguments:
+        id_tipo_envio = Int(required=True)
+        descripcion = String(required=True)
+    
+    tipo_envio = Field(lambda: TipoEnvio)
+
+    def mutate(self, info, id_tipo_envio, descripcion):
+        tipo_envio = TipoEnvioModel(id_tipo_envio=id_tipo_envio, descripcion=descripcion)
+
+        db.session.add(tipo_envio)
+        db.session.commit()
+        
+        return createTipoEnvio(tipo_envio=tipo_envio)
+
 class Mutation(ObjectType):
     create_linea_carrito = createLineaCarrito.Field()
     update_linea_carrito = updateLineaCarrito.Field()
@@ -133,3 +336,14 @@ class Mutation(ObjectType):
     create_usuario = createUsuario.Field()
     create_sesion = createSesion.Field()
     delete_sesion = deleteSesion.Field()
+    create_autor = createAutor.Field()
+    create_ciudad = createCiudad.Field()
+    create_deseo_libro = createDeseoLibro.Field()
+    create_editorial = createEditorial.Field()
+    create_encuadernado = createEncuadernado.Field()
+    create_genero = createGenero.Field()
+    create_libro_editorial = createLibroEditorial.Field()
+    create_libro_genero = createLibroGenero.Field()
+    create_libro_promocion = createLibroPromocion.Field()
+    create_resenia = createResenia.Field()
+    create_tipo_envio = createTipoEnvio.Field()
--- models/query.py
+++ models/query.py
@@ -13,11 +13,51 @@ from graphene import (
 #     Funko,
 #     User,
 # )
-from .objects import Libro, LineaCarrito, Usuario, Sesion
+from .objects import (Libro,
+                    LineaCarrito,
+                    Usuario,
+                    Sesion,
+                    Autor,
+                    Ciudad,
+                    DeseoLibro,
+                    Direccion,
+                    Editorial,
+                    Encuadernado,
+                    FavoritoLibro,
+                    Genero,
+                    LibroAutor,
+                    LibroEditorial,
+                    LibroEncuadernado,
+                    LibroGenero,
+                    LibroPromocion,
+                    LineaPedido,
+                    Pedido,
+                    PromocionDescuento,
+                    Resenia,
+                    TipoEnvio)
+
 from .libro import Libro as LibroModel
 from .linea_carrito import LineaCarrito as LineaCarritoModel
 from .usuario import Usuario as UsuarioModel
 from .sesion import Sesion as SesionModel
+from .autor import Autor as AutorModel
+from .ciudad import Ciudad as CiudadModel
+from .deseo_libro import DeseoLibro as DeseoLibroModel
+from .direccion import Direccion as DireccionModel
+from .editorial import Editorial as EditorialModel
+from .encuadernado import Encuadernado as EncuadernadoModel
+from .favorito_libro import FavoritoLibro as FavoritoLibroModel
+from .genero import Genero as GeneroModel
+from .libro_autor import LibroAutor as LibroAutorModel
+from .libro_editorial import LibroEditorial as LibroEditorialModel
+from .libro_encuadernado import LibroEncuadernado as LibroEncuadernadoModel
+from .libro_genero import LibroGenero as LibroGeneroModel
+from .libro_promocion import LibroPromocion as LibroPromocionModel
+from .linea_pedido import LineaPedido as LineaPedidoModel
+from .pedido import Pedido as PedidoModel
+from .promocion_descuento import PromocionDescuento as PromocionDescuentoModel
+from .resenia import Resenia as ReseniaModel
+from .tipo_envio import TipoEnvio as TipoEnvioModel
 
 
 
@@ -26,6 +66,25 @@ class Query(ObjectType):
     libros_en_carrito = List(lambda: LineaCarrito, id_carrito=Int(), id_producto=Int())
     usuarios = List(lambda: Usuario, id_usuario=String())
     sesiones = List(lambda: Sesion, id_sesion=String())
+    autores = List(lambda: Autor, id_autor=Int(), nombre_autor=String())
+    ciudades = List(lambda: Ciudad, cp=Int(), nombre_ciudad=String())
+    deseos_libro = List(lambda: DeseoLibro, id_usuario=String(), id_libro=Int())
+    direcciones = List(lambda: Direccion, id_direccion=Int(), id_usuario=String())
+    editoriales = List(lambda: Editorial, id_editorial=Int(), nombre_editorial=String())
+    encuadernados = List(lambda: Encuadernado, id_encuadernado=Int(), tipo=String())
+    favoritos_libro = List(lambda: FavoritoLibro, id_usuario=String(), id_libro=Int())
+    generos = List(lambda: Genero, id_genero=Int(), nombre_genero=String())
+    libros_autores = List(lambda: LibroAutor, id_libro=Int(), id_autor=Int())
+    libros_editoriales = List(lambda: LibroEditorial, id_libro=Int(), id_editorial=Int())
+    libros_encuadernados = List(lambda: LibroEncuadernado, id_libro=Int(), id_encuadernado=Int())
+    libros_generos = List(lambda: LibroGenero, id_libro=Int(), id_genero=Int())
+    libros_promociones = List(lambda: LibroPromocion, id_libro=Int(), id_promocion_descuento=Int())
+    lineas_pedidos = List(lambda: LineaPedido, id_pedido=Int(), id_libro=Int())
+    pedidos = List(lambda: Pedido, id_pedido=Int(), id_envio=Int())
+    promociones_descuento = List(lambda: PromocionDescuento, id_promocion_descuento=Int(), nombre_promocion=String())
+    resenias = List(lambda: Resenia, texto=String(), valoracion=Int(), id_usuario=String(), id_libro=Int())
+    tipos_envio = List(lambda: TipoEnvio, id_tipo_envio=Int(), descripcion=String())
+
 
     def resolve_libros(self, info, id=None, precio=None):
         print(info.context.cookies.get("user_id"))
@@ -54,4 +113,152 @@ class Query(ObjectType):
         query = Sesion.get_query(info=info)
         if id_sesion:
             query = query.filter(SesionModel.id_sesion==id_sesion)
+        return query.all()
+    
+    def resolve_autores(self, info, id_autor=None, nombre_autor=None):
+        query = Autor.get_query(info=info)
+        if id_autor:
+            query = query.filter(AutorModel.id_autor == id_autor)
+        if nombre_autor:
+            query = query.filter(AutorModel.nombre_autor == nombre_autor)
+        return query.all()
+
+    def resolve_ciudades(self, info, cp=None, nombre_ciudad=None):
+        query = Ciudad.get_query(info=info)
+        if cp:
+            query = query.filter(CiudadModel.cp == cp)
+        if nombre_ciudad:
+            query = query.filter(CiudadModel.nombre_ciudad == nombre_ciudad)
+        return query.all()
+
+    def resolve_deseos_libro(self, info, id_usuario=None, id_libro=None):
+        query = DeseoLibro.get_query(info=info)
+        if id_usuario:
+            query = query.filter(DeseoLibroModel.id_usuario == id_usuario)
+        if id_libro:
+            query = query.filter(DeseoLibroModel.id_libro == id_libro)
+        return query.all()
+
+    def resolve_direcciones(self, info, id_direccion=None, id_usuario=None):
+        query = Direccion.get_query(info=info)
+        if id_direccion:
+            query = query.filter(DireccionModel.id_direccion == id_direccion)
+        if id_usuario:
+            query = query.filter(DireccionModel.id_usuario == id_usuario)
+        return query.all()
+
+    def resolve_editoriales(self, info, id_editorial=None, nombre_editorial=None):
+        query = Editorial.get_query(info=info)
+        if id_editorial:
+            query = query.filter(EditorialModel.id_editorial == id_editorial)
+        if nombre_editorial:
+            query = query.filter(EditorialModel.nombre_editorial == nombre_editorial)
+        return query.all()
+
+    def resolve_encuadernados(self, info, id_encuadernado=None, tipo=None):
+        query = Encuadernado.get_query(info=info)
+        if id_encuadernado:
+            query = query.filter(EncuadernadoModel.id_encuadernado == id_encuadernado)
+        if tipo:
+            query = query.filter(EncuadernadoModel.tipo == tipo)
+        return query.all()
+
+    def resolve_favoritos_libro(self, info, id_usuario=None, id_libro=None):
+        query = FavoritoLibro.get_query(info=info)
+        if id_usuario:
+            query = query.filter(FavoritoLibroModel.id_usuario == id_usuario)
+        if id_libro:
+            query = query.filter(FavoritoLibroModel.id_libro == id_libro)
+        return query.all()
+
+    def resolve_generos(self, info, id_genero=None, nombre_genero=None):
+        query = Genero.get_query(info=info)
+        if id_genero:
+            query = query.filter(GeneroModel.id_genero == id_genero)
+        if nombre_genero:
+            query = query.filter(GeneroModel.nombre_genero == nombre_genero)
+        return query.all()
+
+    def resolve_libros_autores(self, info, id_libro=None, id_autor=None):
+        query = LibroAutor.get_query(info=info)
+        if id_libro:
+            query = query.filter(LibroAutorModel.id_libro == id_libro)
+        if id_autor:
+            query = query.filter(LibroAutorModel.id_autor == id_autor)
+        return query.all()
+
+    def resolve_libros_editoriales(self, info, id_libro=None, id_editorial=None):
+        query = LibroEditorial.get_query(info=info)
+        if id_libro:
+            query = query.filter(LibroEditorialModel.id_libro == id_libro)
+        if id_editorial:
+            query = query.filter(LibroEditorialModel.id_editorial == id_editorial)
+        return query.all()
+
+    def resolve_libros_encuadernados(self, info, id_libro=None, id_encuadernado=None):
+        query = LibroEncuadernado.get_query(info=info)
+        if id_libro:
+            query = query.filter(LibroEncuadernadoModel.id_libro == id_libro)
+        if id_encuadernado:
+            query = query.filter(LibroEncuadernadoModel.id_encuadernado == id_encuadernado)
+        return query.all()
+
+    def resolve_libros_generos(self, info, id_libro=None, id_genero=None):
+        query = LibroGenero.get_query(info=info)
+        if id_libro:
+            query = query.filter(LibroGeneroModel.id_libro == id_libro)
+        if id_genero:
+            query = query.filter(LibroGeneroModel.id_genero == id_genero)
+        return query.all()
+
+    def resolve_libros_promociones(self, info, id_libro=None, id_promocion_descuento=None):
+        query = LibroPromocion.get_query(info=info)
+        if id_libro:
+            query = query.filter(LibroPromocionModel.id_libro == id_libro)
+        if id_promocion_descuento:
+            query = query.filter(LibroPromocionModel.id_promocion_descuento == id_promocion_descuento)
+        return query.all()
+
+    def resolve_lineas_pedidos(self, info, id_pedido=None, id_libro=None):
+        query = LineaPedido.get_query(info=info)
+        if id_pedido:
+            query = query.filter(LineaPedidoModel.id_pedido == id_pedido)
+        if id_libro:
+            query = query.filter(LineaPedidoModel.id_libro == id_libro)
+        return query.all()
+
+    def resolve_pedidos(self, info, id_pedido=None, id_envio=None):
+        query = Pedido.get_query(info=info)
+        if id_pedido:
+            query = query.filter(PedidoModel.id_pedido == id_pedido)
+        if id_envio:
+            query = query.filter(PedidoModel.id_envio == id_envio)
+        return query.all()
+
+    def resolve_promociones_descuento(self, info, id_promocion_descuento=None, nombre_promocion=None):
+        query = PromocionDescuento.get_query(info=info)
+        if id_promocion_descuento:
+            query = query.filter(PromocionDescuentoModel.id_promocion_descuento == id_promocion_descuento)
+        if nombre_promocion:
+            query = query.filter(PromocionDescuentoModel.nombre_promocion == nombre_promocion)
+        return query.all()
+
+    def resolve_resenias(self, info, texto=None, valoracion=None, id_usuario=None, id_libro=None):
+        query = Resenia.get_query(info=info)
+        if texto:
+            query = query.filter(ReseniaModel.texto == texto)
+        if valoracion:
+            query = query.filter(ReseniaModel.valoracion == valoracion)
+        if id_usuario:
+            query = query.filter(ReseniaModel.id_usuario == id_usuario)
+        if id_libro:
+            query = query.filter(ReseniaModel.id_libro == id_libro)
+        return query.all()
+
+    def resolve_tipos_envio(self, info, id_tipo_envio=None, descripcion=None):
+        query = TipoEnvio.get_query(info=info)
+        if id_tipo_envio:
+            query = query.filter(TipoEnvioModel.id_tipo_envio == id_tipo_envio)
+        if descripcion:
+            query = query.filter(TipoEnvioModel.descripcion == descripcion)
         return query.all()
